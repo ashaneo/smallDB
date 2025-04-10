e6987542d672a47d9a3c1cd2eb3789f99aa80667
ashaneo <subodha162@gmail.com>
2025-04-10 23:19:07 +0530

    nit

--- application.py
+++ application.py
@@ -9,20 +9,11 @@ recover()
 # insert('table1', {"id": 1, "name": "Alice"})
 # insert('table2', {"product_id": 100, "price": 19.99})
 
-# Query inserted data
-# result1 = query('table1', 'id', 1)
-# result2 = query('table2', 'product_id', 100)
-
-# print("Query results:")
-# print(result1)
-# print(result2)
-
 # Creating a new table dynamically
 create_table('table5', ["student_id", "index", "name", "age"], "student_id")
 
 # Drop an existing table
 drop_table('table3')
-
 
 # insert('table3', {"user_id": 200, "email": "john@example.com", "age": 28})
 
